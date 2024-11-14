70f5055985a0ea0e3d15863ebe0159a15c3501fb
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-11-14 13:09:04 -0600

    .

--- runDocker.sh
+++ runDocker.sh
@@ -18,9 +18,5 @@ echo Ejecutando el contenedor del Importador Diario
 docker run --name dailyImporter -p 3017:3016 -d -v /archivo:/c-gn4-importer/archivo daily-importer-v2
 
 echo Hecho
-
-docker exec -it dailyImporter bash
-ls /c-gn4-importer/archivo
-
-# echo Ejecutando la consola
-# docker logs -f dailyImporter
+echo Ejecutando la consola
+docker logs -f dailyImporter
