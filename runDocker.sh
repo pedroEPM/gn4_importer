52d8650494f6980994407048344d42a718007f51
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-11-14 12:59:27 -0600

    .

--- runDocker.sh
+++ runDocker.sh
@@ -14,7 +14,9 @@ echo Creando nueva imagen del Importador Diario
 sleep 1
 docker build -t daily-importer-v2 .
 echo Ejecutando el contenedor del Importador Diario
-docker run --name dailyImporter  -p 3017:3016 -d daily-importer-v2
+# docker run --name dailyImporter  -p 3017:3016 -d daily-importer-v2
+docker run --name dailyImporter -p 3017:3016 -d -v /archivo:/c-gn4-importer/archivo daily-importer-v2
+
 echo Hecho
 echo Ejecutando la consola
 docker logs -f dailyImporter
