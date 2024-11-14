687c2c606cccc436db0b8def8830074802d3f4bf
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-11-14 12:29:24 -0600

    .

--- utils/getPublicIP.js
+++ utils/getPublicIP.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export default async function getPublicIP() {
     try {
         const response = await axios.get('https://api.ipify.org?format=json');
-        console.log(`Tu dirección IP pública es: ${response.data.ip} :D`);
+        console.log(`Tu dirección IP pública es: ${response.data.ip}`);
     } catch (error) {
         console.error('Error obteniendo la IP pública:', error);
     }
