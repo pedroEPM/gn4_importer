be0396a1c040a4870369a7431d94ca75aa6b2fe1
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-11-14 15:38:20 -0600

    :

--- utils/x-thumbs.js
+++ utils/x-thumbs.js
@@ -3,7 +3,7 @@ const folder = process.env.FOLDER + '/archivo-in';
 
 export const moveThumbs = async( cUploadDate ) => {
     try {
-        const files = fs.readdirSync(folder).filter(element => element.trim().toLocaleLowerCase().includes('_Thumb.gif'));
+        const files = fs.readdirSync(folder).filter(element => element.trim().toLocaleLowerCase().includes('_thumb'));
         console.log(files.length)
         // for(const file of files) await fs.promises.rename(folder + '/' + file, folder + '/exported/' + cUploadDate + '/x-thumbs/' + file);
         for(const file of files) await fs.promises.copyFile(folder + '/' + file, folder + '/exported/' + cUploadDate + '/x-thumbs/' + file);
