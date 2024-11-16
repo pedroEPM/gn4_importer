954a0c4704a3fbb5d4c5e8fde6afa14a23ea2294
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-11-16 16:48:41 -0600

    :

--- queries/pdfs.js
+++ queries/pdfs.js
@@ -94,7 +94,7 @@ export const getPDFByUploadedDate = async(date) => {
 
         return await pdf.find({
             uploadDate: new Date(date)
-        }).skip(1000).limit(20)
+        }).skip(1000).limit(100)
         
     } catch (error) {
         console.log('Erro ', error)
