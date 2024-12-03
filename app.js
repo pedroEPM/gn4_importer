ebc5d7f44c63b575530868bf170a0c65ec63e06b
pedroEPM <110569970+pedroEPM@users.noreply.github.com>
2024-12-02 18:03:35 -0600

    .

--- app.js
+++ app.js
@@ -13,6 +13,42 @@ import publicIP from './utils/getPublicIP.js';
 
 const app = express();
 
+
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+    host: "mail.grupomegamedia.mx",
+    port: 26,
+    // host: "192.168.7.13",
+    secure: false,
+    auth: {
+        user: "no-reply-megateca@grupomegamedia.mx",
+        pass: "@X0n4rch1v0#",
+    },
+    // tls: {
+    //     // do not fail on invalid certs
+    //     rejectUnauthorized: false,
+    // },
+})
+
+setTimeout(async () => {
+    try {
+        const cEmail = 'pablo.estrellapalomo@gmail.com';
+        await transporter.sendMail({
+            from: `no-reply-megateca@megamedia.com.mx`,
+            // from: `"No reply" <no-reply-megateca@megamedia.com.mx>`,
+            to: cEmail,
+          
+            subject: 'Reporte: mmmmmmmmmm',
+            // subject: 'Reporte: ' + description,
+            html: `<strong>El usuario:</strong> `
+            // html: `<strong>El usuario:</strong> ${emailNewUser} <br> <strong>Indica el siguiente reporte:</strong> ${description}`
+        })
+    } catch (error) {
+        console.log('Error ', error)
+    }
+}, 2000);
+
 app.use(express.json());
 app.use(logger('dev'))
 app.use(cors());
